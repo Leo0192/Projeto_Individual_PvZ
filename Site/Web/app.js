ad34f3e6f6a027d3fedc7ede62e8aac50ccc057c
Leo0192 <leonardo.tubero@sptech.school>
2025-05-23 14:11:35 -0300

    Exclusão dos arquivos relacionados a aviso

--- Site/Web/app.js
+++ Site/Web/app.js
@@ -18,7 +18,6 @@ var app = express();
 // rotas possiveis para utilizar
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
@@ -32,7 +31,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
