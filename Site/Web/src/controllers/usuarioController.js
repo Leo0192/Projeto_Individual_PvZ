d05f3cd56d6b51ca964f81fe0ce414ce2d983fc6
Leo0192 <leonardo.tubero@sptech.school>
2025-05-23 08:39:42 -0300

    Atualização do cadastro para pleno funcionamento

--- Site/Web/src/controllers/usuarioController.js
+++ Site/Web/src/controllers/usuarioController.js
@@ -1,80 +1,75 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+
+// function cadastrar(req, res) {
+//     var nome = req.body.nomeServer;
+//     var nome_usuario = req.bodynome_usuarioServer;
+//     var email = req.body.emailServer;
+//     var senha = req.body.senhaServer;
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
+    var email = req.body.email;
+    var senha = req.body.senha;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
-    } else {
+    // Faz uma chamada ao banco de dados e traz os dados das tabelas
+    usuarioModel.autenticar(email, senha)
+        .then(
+            function (resultadoAutenticar) {
+                console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-        // Faz uma chamada ao banco de dados e traz os dados das tabelas
-        usuarioModel.autenticar(email, senha)
-            .then(
-                function (resultadoAutenticar) {
-                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                if (resultadoAutenticar.length == 1) {
+                    console.log(resultadoAutenticar);
 
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-
-                        // tem que adicionar o campo que será utilizado/ coluna
-                        // aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                        //     .then((resultadoAquarios) => {
-                        //         if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                    });
-                                // } else {
-                                //     res.status(204).json({ aquarios: [] });
-                                // }
-                            // })
-                    } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
-                    } else {
-                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    }
+                    // tem que adicionar o campo que será utilizado/ coluna
+                    // aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                    //     .then((resultadoAquarios) => {
+                    //         if (resultadoAquarios.length > 0) {
+                    res.json({
+                        id: resultadoAutenticar[0].id,
+                        email: resultadoAutenticar[0].email,
+                        nome: resultadoAutenticar[0].nome,
+                        nome_usuario: resultadoAutenticar[0].nome_usuario,
+                        senha: resultadoAutenticar[0].senha,
+                    });
+                    // } else {
+                    //     res.status(204).json({ aquarios: [] });
+                    // }
+                    // })
+                } else if (resultadoAutenticar.length == 0) {
+                    res.status(403).send("Email e/ou senha inválido(s)");
+                } else {
+                    res.status(403).send("Mais de um usuário com o mesmo login e senha!");
                 }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
-
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
 }
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var nome_usuario = req.body.nome_usuarioServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var telefone = req.body.telefoneServer;
+    var nome = req.body.nome;
+    var nome_usuario = req.body.nome_usuario;
+    var email = req.body.email;
+    var senha = req.body.senha;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    }else if (nome_usuario == undefined) {
+    } else if (nome_usuario == undefined) {
         res.status(400).send("Seu nome de usuario está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (telefone == undefined) {
-        res.status(400).send("seu telefone a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, nome_usuario, email, senha, telefone)
+        usuarioModel.cadastrar(nome, nome_usuario, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -92,7 +87,18 @@ function cadastrar(req, res) {
     }
 }
 
+function listar(req, res) {
+    usuarioModel.listar()
+        .then(resultado => {
+            res.status(200).json(resultado);
+        })
+        .catch(erro => {
+            console.log("Erro ao listar usuários:", erro.sqlMessage || erro);
+            res.status(500).json(erro.sqlMessage || erro);
+        });
+}
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    listar
 }
