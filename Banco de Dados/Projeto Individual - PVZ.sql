56a8634f33ae3de1bc280f6a19e4c8ab5cc37d97
Leo0192 <leonardo.tubero@sptech.school>
2025-05-30 08:24:15 -0300

    Atualização da documentação e upload de imagens

--- Banco de Dados/Projeto Individual - PVZ.sql
+++ Banco de Dados/Projeto Individual - PVZ.sql
@@ -1,71 +1,70 @@
 -- criando o banco de dados
-create database if not exists PIindividual;
+	create database if not exists PIindividual;
 
+	-- colocando o banco de dados em uso
+	use PIindividual;
 
--- colocando o banco de dados em uso
-use PIindividual;
+	-- criando a tabela usuário
+	create table if not exists usuario(
+	id int primary key auto_increment not null,
+	nome varchar (60) not null, -- armazena o nome real do usuario
+	nome_usuario varchar (40) not null, --  armazena o nome de usuarioa de quem está utilizando
+	email varchar (50) not null, -- armazena o email utilizado
+	senha varchar (20) not null -- armazena a senha do usuário
+	);
 
+	-- criando a tabela Inforamções pessoais
+	create table if not exists Informações_pessoais(
+	idUsuario int primary key auto_increment not null, -- referência o o id da tabela Usuario
+	Nome_completo varchar (100), -- armazena o nome completo do usuário
+	sexo_origem char (9), -- armazena o seo de origem do Usuário
+	idade int, --  armazena a idade do usuário
+	País varchar (25), -- registra o seu país de origem
+	foreign key (idUsuario) references Usuario (id), -- adicionando uma foreign key na coluna idUsuario
+	check (sexo_origem in('masculino','feminino')) -- adicionando uma checagem na coluna sexo para: masculino e feminino
+	);
 
--- criando a tabela usuário
-create table if not exists usuario(
-id int primary key auto_increment not null,
-nome varchar (60) not null, -- armazena o nome real do usuario
-nome_usuario varchar (40) not null, --  armazena o nome de usuarioa de quem está utilizando
-email varchar (50) not null, -- armazena o email utilizado
-senha varchar (20) not null -- armazena a senha do usuário
-);
+	-- craindo a tabela Forum onde seram armazenadas as mensagens dos usuários a fim de organizar e manter um certo monitoramento na conversa
+	create table if not exists Forum (
+	idUsuario int primary key auto_increment not null,
+	histórico_mensagem varchar (1000), -- armazena as mensagens de cada usuário  enviadas no fórum
+	status char (7), -- adicionando o status da conta do usuário para ativo (caso tenha logado em até um mês) ou inativo
+	check (status in ('ativo','inativo')) --  adiconando a checagem na coluna status do usuário
+	);
 
--- criando a tabela Inforamções pessoais
-create table if not exists Informações_pessoais(
-idUsuario int primary key auto_increment not null, -- referência o o id da tabela Usuario
-Nome_completo varchar (100), -- armazena o nome completo do usuário
-sexo_origem char (9), -- armazena o seo de origem do Usuário
-idade int, --  armazena a idade do usuário
-País varchar (25), -- registra o seu país de origem
-foreign key (idUsuario) references usuario (id), -- adicionando uma foreign key na coluna idUsuario
-check (sexo_origem in('masculino','feminino')) -- adicionando uma checagem na coluna sexo para: masculino e feminino
-);
-
--- craindo a tabela Forum onde seram armazenadas as mensagens dos usuários a fim de organizar e manter um certo monitoramento na conversa
-create table if not exists Forum (
-idUsuario int primary key auto_increment not null,
-histórico_mensagem varchar (1000), -- armazena as mensagens de cada usuário  enviadas no fórum
-status char (7), -- adicionando o status da conta do usuário para ativo (caso tenha logado em até um mês) ou inativo
-check (status in ('ativo','inativo')) --  adiconando a checagem na coluna status do usuário
-);
-
-CREATE TABLE if not exists aviso (
-	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
+	CREATE TABLE if not exists aviso (
+		idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+		titulo VARCHAR(100),
+		descricao VARCHAR(150),
+		fk_usuario INT,
+		FOREIGN KEY (fk_usuario) REFERENCES Usuario(id)
+	);
 
 -- Inserindo dados na tabela Usuario
 
--- INSERT INTO usuario (nome, nome_usuario, email, senha)
+INSERT INTO usuario (nome, nome_usuario, email, senha)
+VALUES
 
-
-  -- ('leo1231', 'leoqwdad1', 'leo@gmafagaail.com', 'senhasdafasa123'),
-  -- ('João Souza', 'joaosouza', 'joao.souza@email.com', 'senha456'),
-  -- ('Ana Costa', 'anacosta', 'ana.costa@email.com', 'senha789');
+  ('leo1231', 'leoqwdad1', 'leo@gmafagaail.com', 'senhasdafasa123'),
+  ('João Souza', 'joaosouza', 'joao.souza@email.com', 'senha456'),
+  ('Ana Costa', 'anacosta', 'ana.costa@email.com', 'senha789');
   
  -- Inserindo dados na tabela Informações_Pessoais
- 
- -- INSERT INTO Informações_pessoais (idUsuario, Nome_completo, sexo_origem, idade, País)
-
---  (1, 'Maria Aparecida da Silva', 'feminino', 28, 'Brasil'),
---  (2, 'João Pedro de Souza', 'masculino', 35, 'Brasil'),
---  (3, 'Ana Carolina Costa', 'feminino', 22, 'Brasil');
+  INSERT INTO Informações_pessoais (idUsuario, Nome_completo, sexo_origem, idade, País)
+VALUES
+  (1, 'Maria Aparecida da Silva', 'feminino', 28, 'Brasil'),
+  (2, 'João Pedro de Souza', 'masculino', 35, 'Brasil'),
+  (3, 'Ana Carolina Costa', 'feminino', 22, 'Brasil');
   
   -- Inserindo informações na tabela fórum
   
---  INSERT INTO Forum (idUsuario, histórico_mensagem, status)
-
---  (1, 'Olá a todos! Estou animada para participar do fórum.', 'ativo'),
---  (2, 'Bom dia, pessoal! Alguém mais teve problemas com o login?', 'ativo'),
---  (3, 'Alguém pode me ajudar com a redefinição de senha?', 'inativo');
+  INSERT INTO Forum (idUsuario, histórico_mensagem, status)
+VALUES
+  (1, 'Olá a todos! Estou animada para participar do fórum.', 'ativo'),
+  (2, 'Bom dia, pessoal! Alguém mais teve problemas com o login?', 'ativo'),
+  (3, 'Alguém pode me ajudar com a redefinição de senha?', 'inativo');
 
 -- dados inseridos no commit anterior
   select* from usuario;
+  
+  
